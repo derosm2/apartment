8838a9d1aced017bb2636a06b0926b9c85c62d61
Mike DeRosa <derosm2@gmail.com>
2019-03-13 14:55:04 -0400

    Fix seed rake task

--- lib/apartment/adapters/abstract_adapter.rb
+++ lib/apartment/adapters/abstract_adapter.rb
@@ -166,6 +166,7 @@ module Apartment
       def seed_data
         silence_warnings{ load_or_abort(Apartment.seed_data_file) } if Apartment.seed_data_file
       end
+      alias seed seed_data
 
       def load_or_abort(file)
         if File.exist?(file)
